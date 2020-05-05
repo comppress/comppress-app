f636221f16c3958544ac0353fed405725a8a51fa
patmars <patmars2603@gmail.com>
2020-05-05 20:26:49 +0200

    Added Volley and Button

--- CouscousApp/app/src/main/java/com/example/couscousapp/MainActivity.java
+++ CouscousApp/app/src/main/java/com/example/couscousapp/MainActivity.java
@@ -3,6 +3,18 @@ package com.example.couscousapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +23,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClickButton(View view){
+        // http://arnab.ch/blog/2013/08/asynchronous-http-requests-in-android-using-volley/
+        // http://46.101.203.225:8080/demo/getNewsCategory
+
+        String jsonInput = "{\"numberNews\": 10,\"category\": \"\"}";
+        final String URL = "/volley/resource/12";
+        // Post params to be sent to the server
+        HashMap<String, String> params = new HashMap<String, String>();
+        params.put("token", "AbCdEfGh123456");
+
+        JsonObjectRequest req = new JsonObjectRequest(URL, new JSONObject(params),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            VolleyLog.v("Response:%n %s", response.toString(4));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.e("Error: ", error.getMessage());
+            }
+        });
+
+        // add the request object to the queue to be executed
+        MainActivity.getInstance().addToRequestQueue(req);
+
+        Log.i("info",jsonInput);
+
+    }
+
 }
