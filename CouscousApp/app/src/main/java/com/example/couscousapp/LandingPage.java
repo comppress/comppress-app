806693e071bcb1dde440ed9a68d02dd950a47008
patmars <patmars2603@gmail.com>
2020-06-13 13:17:19 +0200

    Funktionsarmes Login + LandingPage 1.2

--- CouscousApp/app/src/main/java/com/example/couscousapp/LandingPage.java
+++ CouscousApp/app/src/main/java/com/example/couscousapp/LandingPage.java
@@ -3,6 +3,7 @@ package com.example.couscousapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class LandingPage extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class LandingPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
+        Toast.makeText(this, "So... how old are you?", Toast.LENGTH_SHORT).show();
     }
 }
--- CouscousApp/app/src/main/java/com/example/couscousapp/LoginActivity.java
+++ CouscousApp/app/src/main/java/com/example/couscousapp/LoginActivity.java
@@ -2,13 +2,40 @@ package com.example.couscousapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText userEmail;
+    EditText userPassword;
+    Button buttonLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+    }
+
+    public void loginProcess(View view) {
+
+        //LOGIN
+
+        Log.i("Info","Login successful!");
+
+        Intent intent = new Intent(this, LandingPage.class);
+        startActivity(intent);
+
+
     }
 }
