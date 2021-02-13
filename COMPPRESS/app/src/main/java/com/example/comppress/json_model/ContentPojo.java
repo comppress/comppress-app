f43cdecfaafcac810bd48ad5282c5ade73f318a5
loengsje <vu.dlong@gmail.com>
2021-02-13 12:32:22 +0100

    Renaming

---
+++ COMPPRESS/app/src/main/java/com/example/comppress/json_model/ContentPojo.java
@@ -0,0 +1,40 @@
+package com.example.comppress.json_model;
+
+public class ContentPojo {
+
+    private Content content;
+    private String source;
+    private Float rating;
+
+    public void setRating(Float rating) {
+        this.rating = rating;
+    }
+
+    public Float getRating() {
+        if (rating == null) {
+            rating = -1f;
+        }
+        return rating;
+    }
+
+    public ContentPojo(Content content) {
+        this.content = content;
+    }
+
+    public void setContent(Content content) {
+        this.content = content;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public Content getContent() {
+        return content;
+    }
+
+    public String getSource() {
+        return source;
+    }
+}
+
